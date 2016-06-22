fdf90ff5c3251e125a9da69b871a6e7bda04f5ba
Jacopo Cavallarin <jacrin92@gmail.com>
2016-06-22 10:53:34 +0200

    Rimosse invocazioni glNormal3f inutili

--- src/gfx/Drawing.cpp
+++ src/gfx/Drawing.cpp
@@ -16,13 +16,10 @@ void gfx::Drawing::drawFloor(float x1, float x2, float y1, float z1, float z2) {
     glNormal3f(0, 1, 0);
     glTexCoord2f(1, 0);
     glVertex3f(x2, y1, z1);
-    glNormal3f(0, 1, 0);
     glTexCoord2f(1, 1);
     glVertex3f(x2, y1, z2);
-    glNormal3f(0, 1, 0);
     glTexCoord2f(0, 1);
     glVertex3f(x1, y1, z2);
-    glNormal3f(0, 1, 0);
     glTexCoord2f(0, 0);
     glVertex3f(x1, y1, z1);
 
@@ -37,13 +34,10 @@ void gfx::Drawing::drawCeil(float x1, float x2, float y1, float z1, float z2) {
     glNormal3f(0, -1, 0);
     glTexCoord2f(1, 0);
     glVertex3f(x1, y1, z1);
-    glNormal3f(0, -1, 0);
     glTexCoord2f(1, 1);
     glVertex3f(x1, y1, z2);
-    glNormal3f(0, -1, 0);
     glTexCoord2f(0, 1);
     glVertex3f(x2, y1, z2);
-    glNormal3f(0, -1, 0);
     glTexCoord2f(0, 0);
     glVertex3f(x2, y1, z1);
 
@@ -59,62 +53,46 @@ void gfx::Drawing::drawCube(float x1, float x2, float y1, float y2, float z1, fl
     glTexCoord2f(0, 0);
     glVertex3f(x1, y1, z1);
     glTexCoord2f(1, 0);
-    glNormal3f(0, 0, 1);
     glVertex3f(x2, y1, z1);
     glTexCoord2f(1, 1);
-    glNormal3f(0, 0, 1);
     glVertex3f(x2, y2, z1);
     glTexCoord2f(0, 1);
-    glNormal3f(0, 0, 1);
     glVertex3f(x1, y2, z1);
 
     glNormal3f(0, 0, -1);
     glTexCoord2f(1, 0);
     glVertex3f(x1, y1, z2);
     glTexCoord2f(1, 1);
-    glNormal3f(0, 0, -1);
     glVertex3f(x1, y2, z2);
     glTexCoord2f(0, 1);
-    glNormal3f(0, 0, -1);
     glVertex3f(x2, y2, z2);
     glTexCoord2f(0, 0);
-    glNormal3f(0, 0, -1);
     glVertex3f(x2, y1, z2);
 
     glNormal3f(0, 1, 0);
     glVertex3f(x1, y2, z1);
-    glNormal3f(0, 1, 0);
     glVertex3f(x2, y2, z1);
-    glNormal3f(0, 1, 0);
     glVertex3f(x2, y2, z2);
-    glNormal3f(0, 1, 0);
     glVertex3f(x1, y2, z2);
 
     glNormal3f(-1, 0, 0);
     glTexCoord2f(1, 0);
-    glNormal3f(-1, 0, 0);
     glVertex3f(x1, y1, z1);
     glTexCoord2f(1, 1);
-    glNormal3f(-1, 0, 0);
     glVertex3f(x1, y2, z1);
     glTexCoord2f(0, 1);
-    glNormal3f(-1, 0, 0);
     glVertex3f(x1, y2, z2);
     glTexCoord2f(0, 0);
-    glNormal3f(-1, 0, 0);
     glVertex3f(x1, y1, z2);
 
     glNormal3f(1, 0, 0);
     glTexCoord2f(0, 0);
     glVertex3f(x2, y1, z1);
     glTexCoord2f(1, 0);
-    glNormal3f(1, 0, 0);
     glVertex3f(x2, y1, z2);
     glTexCoord2f(1, 1);
-    glNormal3f(1, 0, 0);
     glVertex3f(x2, y2, z2);
     glTexCoord2f(0, 1);
-    glNormal3f(1, 0, 0);
     glVertex3f(x2, y2, z1);
 
 
